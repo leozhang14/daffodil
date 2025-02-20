54017440f8d562d2585102a2c910bae64cfc7c63
Leo Zhang <leozhang53@gmail.com>
2025-02-20 16:24:24 -0500

    feat(demo): add shopify configuration for demo

--- apps/demo/src/app/drivers/shopify.module.ts
+++ apps/demo/src/app/drivers/shopify.module.ts
@@ -1,58 +1,44 @@
-import { HttpHeaders } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { InMemoryCache } from '@apollo/client/core';
 import {
-  InMemoryCache,
-  ApolloLink,
-} from '@apollo/client/core';
-import { onError } from '@apollo/client/link/error';
-import { Apollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
+  Apollo,
+  provideApollo,
+} from 'apollo-angular';
 
+
+import { DaffAuthorizeNetInMemoryDriverModule } from '@daffodil/authorizenet/driver/in-memory';
 import { DaffCartInMemoryDriverModule } from '@daffodil/cart/driver/in-memory';
+import { DaffCategoryInMemoryDriverModule } from '@daffodil/category/driver/in-memory';
+import { DaffInMemoryDriverModule } from '@daffodil/driver/in-memory';
+import { provideShopifyApolloDriver } from '@daffodil/driver/shopify';
+import { DaffNavigationInMemoryDriverModule } from '@daffodil/navigation/driver/in-memory';
 import { DaffNewsletterInMemoryDriverModule } from '@daffodil/newsletter/driver/in-memory';
 import { DaffProductShopifyDriverModule } from '@daffodil/product/driver/shopify';
+import {
+  DaffDefaultProductFactory,
+  provideDaffProductExtraFactoryTypes,
+} from '@daffodil/product/testing';
+
 
 import { environment } from '../../environments/environment';
 import { ShopifyEnviromentDriverConfiguration } from '../../environments/environment.interface';
 
-const cache = new InMemoryCache();
+const domain = (<ShopifyEnviromentDriverConfiguration>environment.driver).domain;
+const accessToken = (<ShopifyEnviromentDriverConfiguration>environment.driver).publicAccessToken;
 
 @NgModule({
   imports: [
-    //Shopify
+    DaffInMemoryDriverModule.forRoot(),
     DaffProductShopifyDriverModule.forRoot(),
     DaffCartInMemoryDriverModule.forRoot(),
     DaffNewsletterInMemoryDriverModule.forRoot(),
+    DaffCategoryInMemoryDriverModule.forRoot(),
+    DaffNavigationInMemoryDriverModule.forRoot(),
+    DaffAuthorizeNetInMemoryDriverModule.forRoot(),
+  ],
+  providers: [
+    provideDaffProductExtraFactoryTypes(DaffDefaultProductFactory),
+    provideShopifyApolloDriver(domain, accessToken),
   ],
 })
-export class DemoDriverModule {
-  driver: ShopifyEnviromentDriverConfiguration = (<ShopifyEnviromentDriverConfiguration>environment.driver);
-
-  // Shopify
-  constructor(apollo: Apollo, httpLink: HttpLink) {
-    apollo.create({
-      link: ApolloLink.from([
-        onError(({ graphQLErrors, networkError }) => {
-          if (graphQLErrors) {
-            graphQLErrors.map(({ message, locations, path }) =>
-              console.log(
-                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-              ),
-            );
-          }
-          if (networkError) {
-            console.log(`[Network error]: ${networkError}`);
-          }
-        }),
-        httpLink.create({
-          uri: this.driver.domain + '/graphql',
-          withCredentials: false,
-          headers: new HttpHeaders({
-            'X-Shopify-Storefront-Access-Token': this.driver.publicAccessToken,
-          }),
-        }),
-      ]),
-      cache,
-    });
-  }
-}
+export class DemoDriverModule {}
