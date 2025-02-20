5c8813aaa8632c5d8d48aeff07604c5043098598
Leo Zhang <leozhang53@gmail.com>
2025-02-20 12:23:24 -0500

    fix(driver): make codegen source documents an array

--- libs/driver/shopify/src/codegen/codegen.ts
+++ libs/driver/shopify/src/codegen/codegen.ts
@@ -2,7 +2,9 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: './shopify-storefront-schema.json',
-  documents: '../../../../../libs/product/driver/shopify/src/queries/gql/**/*.ts',
+  documents: [
+    '../../../../../libs/product/driver/shopify/src/queries/gql/**/*.ts',
+  ],
   generates: {
     './generated-shopify-types.ts': {
       plugins: ['typescript', 'typescript-operations', 'typescript-apollo-angular'],
